cdfa2af7f2dfdf77d088dc847d73e56a7033301e
mary <marymetildasugirthamj@gmail.com>
2026-03-25 21:47:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "my-ec2-vm" {
   # ami               = "ami-0d2614eafc1b0e4d2"  #windows
   instance_type     = "t2.micro"
   #availability_zone = "ap-south-1b"
-  availability_zone = "ap-south-1a"
   tags = {
      "Name" = "web1"
     #"tag1" = "Update-test-1"    
